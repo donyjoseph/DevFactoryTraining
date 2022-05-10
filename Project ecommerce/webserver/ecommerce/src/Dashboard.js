6266a257647afd9ced0cf51cbab9a228664d83bc
donyjoseph <103090208+donyjoseph@users.noreply.github.com>
2022-05-10 17:15:03 +0530

    g
    project

--- Project ecommerce/webserver/ecommerce/src/Dashboard.js
+++ Project ecommerce/webserver/ecommerce/src/Dashboard.js
@@ -5,11 +5,52 @@ var a=[
 {id:"1",Productnmae:"s1",Rate:"30",Tax:"2"},
 {id:"2",Productnmae:"s2",Rate:"60",Tax:"10"},
 {id:"2",Productnmae:"s2",Rate:"60",Tax:"10"},
-]};
-<tr>  <td>item.Id</td>  <td>item.txtprdName</td>  <td>item.Rate</td>  <td>item.Tax</td></tr>
+];
 const {productlist,setproductlist}=useState([
     {id:"1",Productnmae:"s1",Rate:"30",Tax:"2"},
 {id:"2",Productnmae:"s2",Rate:"60",Tax:"10"},
 {id:"2",Productnmae:"s2",Rate:"60",Tax:"10"},
 ]);
 useEffect=>{},[];
+return (
+    <div className="maindiv">
+      <div className="headerpart"> 
+        <label>User</label>
+      </div>
+      <div className="bodypart">
+        <div className="menu">
+          <nav>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Orders</li>
+            <li>Logout</li>
+          </nav>
+        </div>
+        <div className="content">
+          <h1>Products</h1>
+          {
+              productlist.map((item,index)=>{
+                  return <p>{index}{item.txtprdName}</p>
+              })
+          }
+          <table>
+            <thead>
+              <th>Id</th>
+              <th>ProductName</th>
+              <th>Rate</th>
+              <th>Tax</th>
+            </thead>
+            <tbody>
+              {
+                  productlist.map((prdct,pos)=>{
+                      return <tr>  <td>{prdct.Id}</td>  <td>{prdct.txtprdName}</td>  <td>{prdct.Rate}</td>  <td>{prdct.Tax}</td></tr>
+                  })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
---
+++ Project ecommerce/webserver/ecommerce/src/navigation.js
@@ -0,0 +1,23 @@
+import Dashboard from "./Dashboard";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default Navigation;
